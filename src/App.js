5845a56e85a3b0fe4353891300f448035bf31359
all4nfabular <74043621+all4nfabular@users.noreply.github.com>
2021-01-14 14:34:42 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-const App = () => {
+import Routes from "./components/Routes";
+const App = (props) => {
+  const handleNavigatePage = (path) => {
+    props.history.push(path);
+  };
   return (
     <div>
       <h1>App Js</h1>
-      <button>Form1</button>
-      <button>Form2</button>
+      <button onClick={() => handleNavigatePage("/")}>Home</button>
+      <button onClick={() => handleNavigatePage("/form1")}>Form1</button>
+      <button onClick={() => handleNavigatePage("/form2")}>Form2</button>
+      <Routes />
     </div>
   );
 };
